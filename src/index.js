e42f71bbe48c5c648cd6a258b53e25178ea61b37
Austin Papritz <austin@papritz.dev>
2023-09-24 15:03:31 -0700

    further simplified index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import { Suspense } from 'react'
-import { Html } from '@react-three/drei'
-import { Logo } from '@pmndrs/branding'
 import './styles.css'
 import { App } from './App'
 
